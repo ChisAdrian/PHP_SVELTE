e10b96f8aa49e779a0c2c7156d0beba12b7876b8
ChisAdrian <44084674+ChisAdrian@users.noreply.github.com>
2023-10-30 13:49:49 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
     const svelte_script = document.createElement('script');
     svelte_script.type = 'text/javascript';
     svelte_script.defer = true;
-    svelte_script.src = '../PHP_SVELTE/public/build/bundle.js';
+    svelte_script.src = 'public/build/bundle.js';
     document.head.appendChild(svelte_script);
 
 
     const svelte_compiled_stylesheet = document.createElement('link');
     svelte_compiled_stylesheet.rel = 'stylesheet';
-    svelte_compiled_stylesheet.href = '../PHP_SVELTE/public/build/bundle.css';
+    svelte_compiled_stylesheet.href = 'public/build/bundle.css';
     document.head.appendChild(svelte_compiled_stylesheet);
 
 </script>
